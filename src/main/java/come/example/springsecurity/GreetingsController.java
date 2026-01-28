228f9a18e4e212157cb32a817c5e0e5f82df45c3
sarthak0401 <sarthakbokade413@gmail.com>
2026-01-28 22:44:22 +0530

    Learning spring security. Creating custom authentication implementation

---
+++ src/main/java/come/example/springsecurity/GreetingsController.java
@@ -0,0 +1,12 @@
+package come.example.springsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+    @GetMapping("/hello")
+    public String gethello(){
+        return "Hello world";
+    }
+}
