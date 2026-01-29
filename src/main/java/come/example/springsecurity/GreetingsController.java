93fa49163105575bf3aa926771a1e6ae226c23e3
sarthak0401 <sarthakbokade413@gmail.com>
2026-01-29 23:29:07 +0530

    Learnt about Hashing and Password Encoding

--- src/main/java/come/example/springsecurity/GreetingsController.java
+++ src/main/java/come/example/springsecurity/GreetingsController.java
@@ -1,5 +1,6 @@
 package come.example.springsecurity;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,18 @@ public class GreetingsController {
     @GetMapping("/hello")
     public String gethello(){
         return "Hello world";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String greetUser(){
+        return "Hello user!";
+    }
+
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String greetAdmin(){
+        return "Heyy, admin!";
     }
 }
--- src/main/java/come/example/springsecurity/SecurityConfig.java
+++ src/main/java/come/example/springsecurity/SecurityConfig.java
@@ -3,18 +3,45 @@ package come.example.springsecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity  // This is required to allow method based access toggle
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                // .formLogin(Customizer.withDefaults())
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated())
+                // .formLogin(Customizer.withDefaults()) ;
                 .httpBasic(Customizer.withDefaults());
+
+        http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
+        http.csrf(csrf -> csrf.disable());
+
         return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("user1").password("{noop}pass1").roles("USER").build();
+        UserDetails user2 = User.withUsername("user2").password("{noop}pass2").roles("USER").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}adminPass").roles("ADMIN").build();
+
+
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager();
+
+        return new InMemoryUserDetailsManager(user1, user2, admin);
     }
 }
